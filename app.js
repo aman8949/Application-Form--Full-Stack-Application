0071463708662537fcfb1c7ffb96d3590577e534
aman8949 <63228142+aman8949@users.noreply.github.com>
2020-06-03 00:54:45 +0530

    Without Admin Functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const config = require('./config.js');
 const Part = require('./models/part');
 const multer = require('multer');
 const fs = require('fs');
+const pug=require('pug');
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(config.mongoUrl, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -20,26 +21,57 @@ const app = express();
 const hostname = 'localhost';
 const port = 3000;
 
+app.set('view engine', 'pug');
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, 'public')));
-var upload = multer({dest: './public/images'});
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname+'-'+Date.now());
+    }
+});
+
+const imageFileFilter = (req, file, cb) => {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('You can upload only image files!'), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFileFilter});
 
 app.get('/', (req, res) => {
     res.statusCode = 200;
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.post('/register', upload.single('userPhoto'),(req, res) => {
+
+app.post('/preview',upload.single('userPhoto'),(req,res)=>{
+    res.render('preview.pug', {
+        title: 'Verify Your Details',
+        body: JSON.stringify(req.body,null,'\n')  // This is your form data as a JSON object.
+      });
+});
+app.post('/register', (req, res) => {
+    console.log(req.body);
     var myData = new Part(req.body);
     myData.save()
         .then(item => {
-            res.send('Data Received!\n' + JSON.stringify(req.body));
+            res.status(200);
+            res.setHeader("Content-Type","application/json");
+            res.json({message:"You have been registered successfully! Note down your Registration No. for future reference."+"\n"+ item._id,success:true});
         })
         .catch(err => {
-            res.status(400).send("Unable to save to database");
+            res.status(400);
+            res.setHeader("Content-Type","application/json");
+            res.json({message:"Unable to save to database!", success:false});
         });
 })
 app.listen(port, hostname, () => {
